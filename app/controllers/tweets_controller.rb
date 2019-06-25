e5fe5296967233763bbfecc2ad3ed0642b0c9885
Tony Baker <this.tonybaker@gmail.com>
2019-06-25 04:28:42 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,89 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    if Helpers.is_signed_in?(session)
+      @tweets = Tweet.all
+      erb :'/tweets/tweets'
+    else
+      redirect to '/users/login'
+    end
+  end
+
+  get '/tweets/new' do
+    user = Helpers.current_user(session)
+    if user.nil?
+      redirect to '/login'
+    else
+      erb :'/tweets/create_tweet'
+    end
+  end
+
+  post '/tweets' do
+    user = Helpers.current_user(session)
+    if user.nil?
+      redirect to '/login'
+    elsif params[:tweet][:content].empty?
+      redirect to '/tweets/new'
+    else
+      user.tweets.new({content: params[:tweet][:content]})
+      user.save
+    end
+    redirect to 'tweets'
+  end
+
+  get '/tweets/:id' do
+    if Helpers.is_signed_in?(session)
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/show_user_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if !Helpers.is_signed_in?(session)
+      redirect to '/login'
+    end
+    @tweet = Tweet.find_by_id(params[:id])
+    if @tweet.user == Helpers.current_user(session)
+      erb :'/tweets/edit_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    @tweet = Tweet.find_by_id(params[:id])
+    if params[:tweet][:content].empty?
+      redirect to "/tweets/#{@tweet.id}/edit"
+    end
+    @tweet.update(params[:tweet])
+    @tweet.save
+    redirect to "/tweets/#{@tweet.id}"
+  end
+
+  delete '/tweets/:id/delete' do
+    if !Helpers.is_signed_in?(session)
+      redirect to '/login'
+    end
+    @tweet = Tweet.find_by_id(params[:id])
+    if @tweet.user == Helpers.current_user(session)
+      @tweet.delete
+      redirect to '/tweets'
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
---
+++ app/models/concerns/slugged.rb
@@ -0,0 +1,12 @@
+module Slugged
+  module InstanceMethods
+    def slug
+      username.parameterize
+    end
+  end
+  module ClassMethods
+    def find_by_slug(slug)
+      self.all.detect {|i| i.username.parameterize == slug}
+    end
+  end
+end
